ba73ecaba2cd09491896a8645bb437fa38662bec
lucasbueno21 <53531008+lucasbueno21@users.noreply.github.com>
2019-11-09 20:30:31 -0300

    Update Progama_com_o_Blynk.ino

--- Progama_com_o_Blynk/Progama_com_o_Blynk.ino
+++ Progama_com_o_Blynk/Progama_com_o_Blynk.ino
@@ -9,17 +9,25 @@ char auth[] = "vNd2aAXRlbZnR3sSozHSOwSK0VGI6F4R";
 //-------- Configurações de Wi-Fi -----------
 char ssid[] = "NOME_DA_REDE";
 char pass[] = "SENHA";
-
-
+//-------- Pino Virtual -----------
+BLYNK_WRITE(V1){
+  int pinValue = param.asInt();
+  //Serial.println(pinValue);
+  if (pinValue == 1) {
+    
+  }
+}
 
 
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);     // Taxa de transmissão 115200
+  pinMode(23, OUTPUT);           // Relé
+  Blynk.begin(auth, ssid, pass); // TOKEN+REDE+SENHA
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Blynk.run();
 
 }
