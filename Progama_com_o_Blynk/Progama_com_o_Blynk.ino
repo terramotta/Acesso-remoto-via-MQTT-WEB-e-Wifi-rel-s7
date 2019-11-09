0e304c99ccccb9cd6465e5ae95c86a598eb1b1cb
lucasbueno21 <53531008+lucasbueno21@users.noreply.github.com>
2019-11-09 19:48:26 -0300

    Create Progama_com_o_Blynk.ino

---
+++ Progama_com_o_Blynk/Progama_com_o_Blynk.ino
@@ -0,0 +1,25 @@
+// Projeto Curto Circuito – ESP32, Button Blynk
+//-------- Bibliotecas -----------
+#define BLYNK_PRINT Serial // Blynk Serial Print
+#include <WiFi.h> // Wi-Fi
+#include <WiFiClient.h> // Wi-Fi client 
+#include <BlynkSimpleEsp32.h> // Blynk-ESP32
+//-------- Token de Autenticação -----------
+char auth[] = "INSIRA_O_TOKEN";
+//-------- Configurações de Wi-Fi -----------
+char ssid[] = "NOME_DA_REDE";
+char pass[] = "SENHA";
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
